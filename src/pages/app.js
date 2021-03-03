3e57787c30bc63fd1ab5d817f09489005ce2e3d6
Erik Schwartz <erik@erikschwartz.net>
2021-03-03 11:22:57 +0000

    Add react leaftlet WIP

--- src/pages/app.js
+++ src/pages/app.js
@@ -6,7 +6,9 @@ import Home from "../components/Home"
 import Login from "../components/Login"
 import SignUp from "../components/SignUp"
 import PrivateRoute from "../components/PrivateRoute"
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
+const position = [51.505, -0.09]
 
 const App = () => (
   <Layout>
@@ -16,6 +18,17 @@ const App = () => (
       <Login path="/app/login" />
       <SignUp path="/app/signup" />
     </Router>
+    <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+      </Marker>
+    </MapContainer>
   </Layout>
 )
 
